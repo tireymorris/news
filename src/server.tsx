7556b51d46cece99ca8d652129359f4664f34f2a
Tirey Morris <tirey@hey.com>
2024-07-04 10:50:08 -0500

    reduce debounce

--- src/server.tsx
+++ src/server.tsx
@@ -28,7 +28,7 @@ app.get("/", async (c) => {
           target="#articles"
           limit="25"
           offset="0"
-          debounce="50"
+          debounce="15"
           class="w-full px-2"
           data-total="1000"
         ></div>
